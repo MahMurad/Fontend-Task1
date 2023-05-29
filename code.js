0264ce8a98b46e5528db8721a6d09a7d6613117a
MahMurad <52890377+MahMurad@users.noreply.github.com>
2023-05-29 21:02:17 +0600

    Update code.js

--- code.js
+++ code.js
@@ -1,7 +1,6 @@
-// JavaScript code
 const button = document.getElementById("myButton");
 const output = document.getElementById("output");
 
 button.addEventListener("click", function() {
-  output.textContent = "Hello, World!";
+  output.textContent = "Greatings!";
 });
